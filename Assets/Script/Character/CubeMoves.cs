548e930165296f223cbc589374ad9bb0388e010e
MetaDrow <106996152+MetaDrow@users.noreply.github.com>
2023-04-06 14:52:21 +0300

    Add jelly cube

---
+++ Assets/Script/Character/CubeMoves.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CubeMoves : MonoBehaviour
+{
+    [SerializeField] KeyCode KeyOne;
+    [SerializeField] KeyCode KeyTwo;
+    [SerializeField] Vector3 MoveDirection;
+    
+    [SerializeField] Rigidbody CubeRigidBody;
+    [SerializeField] Rigidbody CubeRigidBody2;
+	[SerializeField] Rigidbody CubeRigidBody3;
+	[SerializeField] Rigidbody CubeRigidBody4;
+
+    [SerializeField] public float MoveForce = 0.2f;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+    private void Update()
+    {
+        Restart();
+    }
+    private void Move()
+    {
+        if (Input.GetKey(KeyOne))
+        {
+            CubeRigidBody.velocity -= MoveDirection * MoveForce;
+            CubeRigidBody2.velocity -= MoveDirection * MoveForce;
+			CubeRigidBody3.velocity -= MoveDirection * MoveForce;
+			CubeRigidBody4.velocity -= MoveDirection * MoveForce;
+        }
+        if (Input.GetKey(KeyTwo))
+        {
+            CubeRigidBody.velocity += MoveDirection * MoveForce;
+            CubeRigidBody2.velocity += MoveDirection * MoveForce;
+			CubeRigidBody3.velocity += MoveDirection * MoveForce;
+			CubeRigidBody4.velocity += MoveDirection * MoveForce;
+        }
+    }
+    private void Restart()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
